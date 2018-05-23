e4de6252c0995c1ad272481b2c805479303f916d
stu@creativewax.co.uk <stu@creativewax.co.uk>
2018-05-23 14:16:27 +0100

    removed sb directly from this tool as its been added globally to the CMS

--- src/containers/schedules/list.js
+++ src/containers/schedules/list.js
@@ -5,7 +5,7 @@ import { actions as notifActions } from 'redux-notifications';
 import List from '../../components/list';
 import { duplicateSchedule, deleteSchedule } from './actions';
 import { truncate } from '../../lib/utils';
-import { Spacebrew } from '../../lib/sb';
+// import { Spacebrew } from '../../lib/sb';
 
 const mapStateToProps = state => {
   const { schedules } = state || [];
@@ -26,24 +26,24 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
   const { dispatch } = dispatchProps;
   const { schedules } = stateProps;
   const { serverIp } = stateProps;
-  const sb = new Spacebrew.Client({ reconnect: true });
-  sb.server = serverIp;
-  sb.port = 9000;
-  sb.name = 'Schedule editor';
-
-  // add pubishers
-  sb.addPublish('OnScheduleUpdateWithJsonString', 'string');
-
-  // callbacks
-  sb.onOpen = onOpen;
-
-  // check when spacebrew is open
-  function onOpen() {
-  console.log('Spacebrew has opened : ' + sb.name);
-  }
-
-  // connect spacebrew
-  sb.connect();
+  // const sb = new Spacebrew.Client({ reconnect: true });
+  // sb.server = serverIp;
+  // sb.port = 9000;
+  // sb.name = 'Schedule editor';
+  //
+  // // add pubishers
+  // sb.addPublish('OnScheduleUpdateWithJsonString', 'string');
+  //
+  // // callbacks
+  // sb.onOpen = onOpen;
+  //
+  // // check when spacebrew is open
+  // function onOpen() {
+  // console.log('Spacebrew has opened : ' + sb.name);
+  // }
+  //
+  // // connect spacebrew
+  // sb.connect();
 
   return {
     ...stateProps,
@@ -66,7 +66,7 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
       const jsonItem = JSON.stringify(schedule);
       console.log(jsonItem);
       try {
-        sb.send('OnScheduleUpdateWithJsonString', 'string', 'raw|' + jsonItem);
+        window.sb.send('OnScheduleUpdateWithJsonString', 'string', 'raw|' + jsonItem);
       } catch (e) {
         console.log(e);
       }
